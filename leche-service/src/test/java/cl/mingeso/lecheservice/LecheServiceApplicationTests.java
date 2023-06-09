fb79500b31f2b75df67b715294907967b598962c
JimCarrasc0 <jaime.carrasco.q@usach.cl>
2023-06-09 12:19:45 -0400

    Añadidos los microservicios básicos

---
+++ leche-service/src/test/java/cl/mingeso/lecheservice/LecheServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.mingeso.lecheservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LecheServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
